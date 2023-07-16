5b4a89943def4546d96bc6f50ecccd7d5a13448f
SHIVAM SINGH <shivamdotsingh@gmail.com>
2023-07-16 21:10:54 +0530

    Added Light and Dark Theme

--- src/App.js
+++ src/App.js
@@ -1,49 +1,33 @@
-import './App.css';
 
-import React, { Component } from 'react'
+
+import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-  pageSize = 5;
-  apiKey = process.env.REACT_APP_NEWS_API
-
-  state = {
-    progress:0
-  }
-
-  setProgress = (progress)=>{
-    this.setState({progress: progress})
-  }
-
-  render() {
-    return (
-      <div>
-        <Router>
-        <NavBar/> 
+const App = () => {
+  const [progress, setProgress] = useState(0)
+  return (
+    <div>
+      <BrowserRouter>
+        <NavBar />
         <LoadingBar
-        height={3}
-        color='#f11946'
-        progress={this.state.progress} 
-      />
-        <Switch>
-          <Route exact path="/"><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/business"><News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business"/></Route> 
-          <Route exact path="/entertainment"><News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route> 
-          <Route exact path="/general"><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/health"><News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health"/></Route> 
-          <Route exact path="/science"><News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science"/></Route> 
-          <Route exact path="/sports"><News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route> 
-          <Route exact path="/technology"><News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route> 
-        </Switch>
-        </Router>
-      </div>
-    )
-  }
+          color='#f11946'
+          progress={progress}
+        />
+        <Routes>
+          <Route path="" element={<News setProgress={setProgress} key="" pagesize={12} country={'in'} category={"general"} />} />
+          <Route path="Sports" element={<News setProgress={setProgress} key="sports" pagesize={12} country={'in'} category={"sports"} />} />
+          <Route path="Business" element={<News setProgress={setProgress} key="business" pagesize={12} country={'in'} category={"business"} />} />
+          <Route path="Entertainment" element={<News setProgress={setProgress} key="entertainment" pagesize={12} country={'in'} category={"entertainment"} />} />
+          <Route path="Technology" element={<News setProgress={setProgress} key="technology" pagesize={12} country={'in'} category={"technology"} />} />
+          <Route path="Science" element={<News setProgress={setProgress} key="science" pagesize={12} country={'in'} category={"science"} />} />
+          <Route path="Health" element={<News setProgress={setProgress} key="health" pagesize={12} country={'in'} category={"health"} />} />
+          <Route path="General" element={<News setProgress={setProgress} key="general" pagesize={12} country={'in'} category={"general"} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,83 @@
-import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Dark from './Dark.png'
+import Light from './Light.png'
+const Navbar = () => {
+  const [mode, setMode] = useState('Light');
 
-export class NavBar extends Component {
-
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-                             
-                          
-                        </ul>
-                        </div>
-                    </div>
-                    </nav>
-            </div>
-        )
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    
+    } else {
+      setMode('dark');
+      document.body.style.backgroundColor = '#333333';
+     
     }
-}
+  };
 
-export default NavBar
+  return (
+    <div>
+        <nav className={`navbar navbar-expand-lg ${mode==='light'?'navbar-dark bg-dark':'navbar-light bg-light'}`}>
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            NewsMonkey
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/Business">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Entertainment">
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Science">
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Technology">
+                  Technology
+                </Link>
+              </li>
+            </ul>
+            </div>
+            <img  src={mode === 'light' ? Dark : Light} onClick={toggleMode} alt="imahe" style={{  width: '39px',  backgroundColor: 'white', padding:'5px' }}  >
+            </img>
+          
+          
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
